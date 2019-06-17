7b22a870d45549246d5a5e5afc365cf3515d19d5
Longshilin <583297550@qq.com>
2019-06-17 09:56:16 +0800

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //开始游戏触发按钮事件
+    public void OnStartGame(int sceneIndex)
+    {
+        Application.LoadLevel(sceneIndex);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 100;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed); // 坐标轴y轴方向 * 每帧的时间 * 速度 = 每帧绕y轴旋转90度
+    }
+
+    public void ChangeSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+}
